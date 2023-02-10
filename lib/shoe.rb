3c9b21b2a961ac835e771adc44c470eddcf91089
rose534 <wambuirose534@gmail.com>
2023-02-10 20:29:17 +0300

    lab

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,12 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand = "Adidas")
+      @brand = brand
+      @color = ""
+      @size = 0
+      @material = ""
+      @condition = ""
+    end
+  
+   
+  end
